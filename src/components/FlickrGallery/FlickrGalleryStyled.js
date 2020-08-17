520d4481ec7c8b465208cf0baa9ff14587870839
Daniel Lupascu <daniellupascu10@gmail.com>
2020-08-17 22:13:17 +0200

    add magnification effect

--- src/components/FlickrGallery/FlickrGalleryStyled.js
+++ src/components/FlickrGallery/FlickrGalleryStyled.js
@@ -2,6 +2,7 @@ import styled from "styled-components/macro";
 
 export const FlickrGalleryContainer = styled.div`
   margin: 20px;
+  position: relative;
 
   display: grid;
   grid-template-columns: repeat(4, minmax(150px, 1fr));
@@ -15,4 +16,11 @@ export const FlickrImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+
+  &:hover {
+    transform: scale(1.5);
+    transition: transform 0.2s;
+    box-shaddow: 0px 24px 38px 3px rgba(0, 0, 0, 0.5),
+      0px 9px 46px 8px rgba(0, 0, 0, 0.5), 0px 11px 15px -7px rgba(0, 0, 0, 0.5);
+  }
 `;
